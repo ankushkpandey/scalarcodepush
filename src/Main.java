86c352a6d7eb85495c55f650effac390ddf4bd30
ankush kumar <ankushkumar78900@gmail.com>
2024-03-28 16:23:22 +0530

    Initial Version 2

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i*3);
             System.out.println("i = " + i*4);
             System.out.println("i = " + i*5);
+            System.out.println("i = " + i*6);
 
         }
     }
